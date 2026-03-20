ac37f572b66b15a136eba179a0b11a4ea91f7e60
Lorenzo_Rocco <lorenzo.rocco@edu-its.it>
2026-03-20 14:24:14 +0100

    updated the icon for business unit to resamble a more correct rappresentation of business. patched various problem regarding the confusion when an error occur. now the correct message comes alerting about error or asking to confirm the decision taken. minor update for various file for a more beatiful to look experience. lastly added the possibility to filter on asset type and business unit the same way you can already can do on user list.

--- src/app/features/asset-type-list/asset-type-list.ts
+++ src/app/features/asset-type-list/asset-type-list.ts
@@ -1,20 +1,20 @@
-import { ChangeDetectorRef, Component, DestroyRef, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, computed, DestroyRef, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { AssetType } from '../../shared/services/asset-type.service';
 import { ApiService } from '../../services/api';
 import { FormsModule } from "@angular/forms";
 import { Subject } from 'rxjs';
-
-//TODO creare un onCloseDialog che ricarica la lista dei tipi senza dover usare una get request
+import { PaginationComponent } from "../../shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-asset-type-list',
-  imports: [FormsModule],
+  imports: [FormsModule, PaginationComponent],
   templateUrl: './asset-type-list.html',
   styleUrl: './asset-type-list.css',
 })
 export class AssetTypeList {
   assetTypes = signal<AssetType[]>([]);
   editableAssetType: AssetType | null = null;
+  filteredAssetType = signal<AssetType[]>([]);
   reload$ = new Subject<boolean>();
   newAssetType: AssetType = {
     id: 0,
@@ -26,64 +26,81 @@ export class AssetTypeList {
   };
 
   initialName = '';
+  filterName= '';
+  controlName = '';
   HDCheck = false;
+  filterHD = false;
   RamCheck = false;
+  filterRam = false;
+  disactiveHDandRam = false;
+  activateHDandRam = false;
+
   isVisible = signal(true);
   loading = signal(true);
+  touched = false;
+
+  alertTitle = '';
   private destroyRef = inject(DestroyRef);
+  errorMessage = 'Nessun Errore.';
   
   @ViewChild('myDialog') dialog!: ElementRef<HTMLDialogElement>;
   @ViewChild('newDialog') newDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild('alertDialog') alertDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild('errorDialog') errorDialog!: ElementRef<HTMLDialogElement>;
+
   // Settaggio del assetTypes
   constructor(private apiService: ApiService, private cdr: ChangeDetectorRef){
     const subscription = this.apiService.getAssetTypes().subscribe({
       next: (types) => {
         this.assetTypes.set(types ?? []);
+        this.filteredAssetType.set(this.assetTypes())
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('API error asset types', err);
+        this.errorMessage = 'Errore con l\'API dell\'asset type: ' + err;
+        this.onErrorOccur();
         this.assetTypes.set([]);
         this.loading.set(false);
       }
     })
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
-  // Cambia lo status dell asset rendendolo attivo o disattivo
-  changeStatus(assetType: AssetType){
-    this.editableAssetType = assetType;
-    const confirmed = window.confirm('Conferma cambiamento?');
-    if(confirmed){
-      if(this.editableAssetType.active === false){
-      this.editableAssetType.active = true;
-      }
-      else{
-        if(this.editableAssetType.active === true){
-        this.editableAssetType.active = false;
-        }
-      }
-    }
-    this.apiService. putAssetActiveChangeById(this.editableAssetType.code, this.editableAssetType.active)
-    .subscribe({
-      next: (updatedAssetType) => {
-        const updatedList = this.assetTypes().map(at => at.code === updatedAssetType.code ? updatedAssetType : at);
-        this.assetTypes.set(updatedList);
-        this.reloadDiv();
-      },
-      error: (err) => {
-        console.error('Errore durante l\'aggiornamento dell\'attivazione del tipo di asset: ', err);
-      }
-    });
-    
+  currentPage = signal(1);
+  itemsPerPage = signal(5);
+
+  // ricalcolo e aggiorno automaticamente dopo ogni cambiamento
+  totalPages = computed(() => {
+    return Math.ceil(this.filteredAssetType().length / this.itemsPerPage());
+  });
+
+  // si aggiorna automaticamente quando cambi pagina o aggiungi/rimuovi users
+  paginatedAssetType = computed(() => {
+    const start = (this.currentPage() - 1) * this.itemsPerPage();
+    const end = start + this.itemsPerPage();
+    return this.filteredAssetType().slice(start, end);
+  });
+
+  //creazione stringa display range.
+  displayRange = computed(() => {
+    const start = (this.currentPage() - 1) * this.itemsPerPage() + 1;
+    const end = Math.min(this.currentPage() * this.itemsPerPage(), this.filteredAssetType().length);
+    return `Mostrando ${start}-${end} di ${this.filteredAssetType().length}`;
+  });
+
+  goToPage(page: number): void {
+    this.currentPage.set(page);
   }
+
   // Diverse funzioni che si attivano prima durante o dopo la modifica di un tipo
   OnOpenDialog(assetType: AssetType){
     this.editableAssetType = assetType;
     this.initialName = assetType.name;
+    this.controlName = assetType.name;
     this.HDCheck = assetType.hardDisk;
     this.RamCheck = assetType.ram;
 
     this.dialog.nativeElement.showModal();
+    this.alertTitle = 'Conferma la modifica di questo asset type?';
   }
   onModify(){
     if(this.editableAssetType !== null){
@@ -96,9 +113,7 @@ export class AssetTypeList {
     }
   }
   onConfirmEdit(){
-    const confirmed = window.confirm('Conferma modifica?');
-
-    if(this.editableAssetType !== null && confirmed){
+    if(this.editableAssetType !== null){
       const puttableAssetType = { 
         name: this.editableAssetType.name, 
         hardDisk: this.editableAssetType.hardDisk, 
@@ -109,24 +124,33 @@ export class AssetTypeList {
         next: (updatedAssetType) => {
           const updatedList = this.assetTypes().map(at => at.code === updatedAssetType.code ? updatedAssetType : at);
           this.assetTypes.set(updatedList);
+
           this.dialog.nativeElement.close();
           this.reloadDiv();
+          this.alertTitle = '';
+          this.controlName = '';
         },
         error: (err) => {
-          console.error('Errore durante l\'aggiornamento del tipo di asset: ', err);
+          this.errorMessage = 'Errore durante l\'aggiornamento del tipo di asset: ' + err;
+          this.onErrorOccur();
           this.dialog.nativeElement.close();
+          this.alertDialog.nativeElement.close();
+          this.alertTitle = '';
+          this.controlName = '';
         }
       });
     }
   }
   onCloseDialog(){
+    this.touched = false;
     const subscription = this.apiService.getAssetTypes().subscribe({
       next: (types) => {
         this.assetTypes.set(types ?? []);
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('API error asset types', err);
+        this.errorMessage = 'API error asset types: ' + err;
+        this.onErrorOccur();
         this.assetTypes.set([]);
         this.loading.set(false);
       }
@@ -136,6 +160,74 @@ export class AssetTypeList {
     this.reloadDiv();
     this.dialog.nativeElement.close();
   }
+
+  // funzione che filtra la lista di asset type
+  onFilter() {
+  let filtered = this.assetTypes();
+
+  if (this.filterName !== '') {
+    const searchName = this.filterName.toLowerCase();
+    filtered = filtered.filter(asset =>
+      asset.name.toLowerCase().includes(searchName)
+    );
+  }
+
+  // filtra solo quelli con entrambi
+  if (this.activateHDandRam) {
+    filtered = filtered.filter(a => a.hardDisk && a.ram);
+  }
+
+  //filtra solo quando nè hard disk nè ram sono presenti
+  else if (this.disactiveHDandRam) {
+      filtered = filtered.filter(a => !a.hardDisk && !a.ram);
+  }
+
+  //filtra le singole opzioni di hard disk e ram
+  else{
+    if (this.filterHD) filtered = filtered.filter(a => a.hardDisk);
+    if (this.filterRam) filtered = filtered.filter(a => a.ram);
+  }
+
+  this.filteredAssetType.set(filtered);
+  this.currentPage.set(1);
+}
+
+// Deseleziona tutte le altre checkbox
+onDisactiveChange() {
+  if (this.disactiveHDandRam) {
+    this.filterHD = false;
+    this.filterRam = false;
+    this.activateHDandRam = false;
+  }
+  this.onFilter();
+}
+
+onHDorRamChange() {
+
+  // Se si attiva HD o Ram, disattiva "disattiva HD e Ram"
+  if (this.filterHD || this.filterRam) {
+    this.disactiveHDandRam = false;
+  }
+
+  // Se entrambi selezionati disattivali e attiva l'apposito
+  if (this.filterHD && this.filterRam) {
+    this.activateHDandRam = true;
+    this.filterHD = false;
+    this.filterRam = false;
+  }
+  this.onFilter();
+}
+
+// disattiva tutte le altre checkbox quando attivo "attiva hard disk e ram"
+onActivateBothChange() {
+  if (this.activateHDandRam) {
+    this.filterHD = false;
+    this.filterRam = false;
+    this.disactiveHDandRam = false;
+  }
+  this.onFilter();
+}
+
   // Ricarica della tabella dei tipi per rendere subito visibile i cambiamenti fatti
   reloadDiv() {
     this.isVisible.set(false);
@@ -144,6 +236,7 @@ export class AssetTypeList {
       this.reload$.next(true);
     }, 0);
   }
+
   // Diverse funzioni che si attivano prima durante o dopo la creazione di un tipo
   OnOpenNewDialog(){
     this.newDialog.nativeElement.showModal();
@@ -151,6 +244,8 @@ export class AssetTypeList {
     this.initialName = '';
     this.HDCheck = false;
     this.RamCheck = false;
+
+    this.alertTitle = 'Conferma la creazione di un nuovo asset Type?';
   }
   onAdd(){
     let createdName = this.initialName.toLowerCase();
@@ -161,39 +256,95 @@ export class AssetTypeList {
     this.newAssetType.ram = this.RamCheck;
   }
   onConfirmCreate(){
-    const confirmed = window.confirm('Conferma creazione?');
-    if(confirmed){
-      const postableAssetType = { 
-        name: this.newAssetType.name,
-        hardDisk: this.newAssetType.hardDisk,
-        ram: this.newAssetType.ram
-      };
+    const postableAssetType = { 
+      name: this.newAssetType.name,
+      hardDisk: this.newAssetType.hardDisk,
+      ram: this.newAssetType.ram
+    };
+    if(this.controlRepeatedName(postableAssetType.name.trim())){
       this.apiService.postAssetType(postableAssetType)
       .subscribe({
         next: (createdAssetType) => {
           const updatedList = [...this.assetTypes(), createdAssetType];
           this.assetTypes.set(updatedList);
+
           this.newDialog.nativeElement.close();
           this.cdr.detectChanges();
           this.reloadDiv();
+          this.alertTitle = '';
         },
         error: (err) => {
-          console.error('Errore durante la creazione del tipo di asset: ', err);
+          this.errorMessage = 'Errore durante la creazione del tipo di asset: ' + err;
+          this.onErrorOccur();
+
           this.newDialog.nativeElement.close();
+          this.alertTitle = '';
         }
       })
     }
+    else{
+      this.errorMessage = 'Errore nella creazione del tipo di asset, non si può usare lo stesso nome per più asset'
+      this.newDialog.nativeElement.close();
+      this.onErrorOccur();
+    }
   }
   onCloseNewDialog(){
+    this.touched = false;
     this.newDialog.nativeElement.close();
     this.initialName = '';
     this.HDCheck = false;
     this.RamCheck = false;
   }
-  isInvalid(){
-    if(this.initialName.length > 2 && this.initialName.match(/^[a-zA-Z]+$/)){
-        return false;
-      }
+
+  // controllo che vengano messe le giuste credenziali
+  isInvalid(): boolean {
+    return !(this.initialName.length > 2 && this.initialName.trim() !== this.controlName);
+  }
+  get specifiedMessage(): string {
+    if (this.initialName.length <= 2) {
+      return 'Nome non valido. Deve essere almeno 2 caratteri';
+    }
+    return 'Nome non valido. Il nome inserito deve essere diverso da quello già presente';
+  }
+  specifiedError(): boolean {
+    return this.initialName.length > 0 && this.isInvalid();
+  }
+  controlRepeatedName(postableAssetTypeName: string){
+    if(this.assetTypes().find(asset => (asset.name === postableAssetTypeName))){
+      return false;
+    }
     else{ return true; }
+  }
+  
+  //creazione di dialogi di avvertenza
+  onAlertDialogOpen(){
+    this.alertDialog.nativeElement.showModal();
+  }
+  onAlertDialogClose(){
+    this.alertDialog.nativeElement.close();
+    this.alertTitle = '';
+  }
+  onChoosingPath(){
+    if(this.alertTitle === 'Conferma la creazione di un nuovo asset Type?'){
+      this.onConfirmCreate();
+    }
+    else{
+      if(this.alertTitle === 'Conferma la modifica di questo asset type?'){
+        this.onConfirmEdit();
+      }
+      else{
+        this.errorMessage = 'errore nella conferma';
+        this.onErrorOccur();
+      }
+    }
+    this.alertDialog.nativeElement.close();
+  }
+
+  //creazione di dialogo di errore
+  onErrorOccur(){
+    this.errorDialog.nativeElement.showModal();
+  }
+  onErrorWindowsClose(){
+    this.errorDialog.nativeElement.close();
   }
 }
--- src/app/features/business-unit/business-unit-list.ts
+++ src/app/features/business-unit/business-unit-list.ts
@@ -3,26 +3,43 @@ import { ApiService } from '../../services/api';
 import { BusinessUnit } from '../../shared/services/business-unit.service';
 import { FormsModule } from "@angular/forms";
 import { Subject } from 'rxjs';
+import { PaginationComponent } from "../../shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-business-unit-list',
-  imports: [FormsModule],
+  imports: [FormsModule, PaginationComponent],
   templateUrl: './business-unit-list.html',
   styleUrl: './business-unit-list.css',
 })
 export class BusinessUnitList {
   businessUnits = signal<BusinessUnit[]>([]);
+  filteredBusinessUnits = signal<BusinessUnit[]>([]);
   loading = signal(true);
   reload$ = new Subject<boolean>();
   private destroyRef = inject(DestroyRef);
 
   @ViewChild('myDialog') dialog!: ElementRef<HTMLDialogElement>;
   @ViewChild('newDialog') newDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild('alertDialog') alertDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild('alertStatusDialog') alertStatusDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild('errorDialog') errorDialog!: ElementRef<HTMLDialogElement>;
 
   editableBusinessUnit: BusinessUnit|null = null
+  controlName = '';
   initialName = '';
+  filterName = '';
   isVisible = signal(true);
   newBusinessUnit : BusinessUnit = {
+    id: 0,
+    code: '',
+    active: true,
+    name: ''
+  };
+
+  errorMessage = 'Nessun errore.';
+  touched = false;
+  alertTitle = '';
+  selectedBusinessUnit: BusinessUnit|null = {
     id: 0,
     code: '',
     active: true,
@@ -34,28 +51,50 @@ export class BusinessUnitList {
     const subscription = this.apiService.getBusinessUnits().subscribe({
       next: (types) => {
         this.businessUnits.set(types ?? []);
+        this.filteredBusinessUnits.set(this.businessUnits());
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('API error asset types', err);
+        this.errorMessage = 'API error asset types' + err;
+        this.onErrorOccur();
         this.businessUnits.set([]);
         this.loading.set(false);
       }
     })
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
+  currentPage = signal(1);
+  itemsPerPage = signal(5);
+
+  // ricalcolo e aggiorno automaticamente dopo ogni cambiamento
+  totalPages = computed(() => {
+    return Math.ceil(this.filteredBusinessUnits().length / this.itemsPerPage());
+  });
+  // si aggiorna automaticamente quando cambi pagina o aggiungi/rimuovi users
+  paginatedBusinessUnit = computed(() => {
+    const start = (this.currentPage() - 1) * this.itemsPerPage();
+    const end = start + this.itemsPerPage();
+    return this.filteredBusinessUnits().slice(start, end);
+  });
+  //creazione stringa display range.
+  displayRange = computed(() => {
+    const start = (this.currentPage() - 1) * this.itemsPerPage() + 1;
+    const end = Math.min(this.currentPage() * this.itemsPerPage(), this.filteredBusinessUnits().length);
+    return `Mostrando ${start}-${end} di ${this.filteredBusinessUnits().length}`;
+  });
+
+  goToPage(page: number): void {
+    this.currentPage.set(page);
+  }
   // Cambia lo status della business unit rendendola attiva o disattiva
   changeStatus(businessUnit: BusinessUnit){
     this.editableBusinessUnit = businessUnit;
-    const confirmed = window.confirm('Conferma cambiamento?');
-    if(confirmed){
-      if(this.editableBusinessUnit.active === false){
+    if(this.editableBusinessUnit.active === false){
       this.editableBusinessUnit.active = true;
-      }
-      else{
-        if(this.editableBusinessUnit.active === true){
+    }
+    else{
+      if(this.editableBusinessUnit.active === true){
         this.editableBusinessUnit.active = false;
-        }
       }
     }
     this.apiService. putBusinessActiveChangeById(this.editableBusinessUnit.code, this.editableBusinessUnit.active)
@@ -66,16 +105,32 @@ export class BusinessUnitList {
         this.reloadDiv();
       },
       error: (err) => {
-        console.error('Errore durante l\'aggiornamento dell\'attivazione della business unit: ', err);
+        this.errorMessage = 'Errore durante l\'aggiornamento dell\'attivazione della business unit: ' + err;
+        this.onErrorOccur();
       }
     });
+  }
+  // funzione che filtra la lista delle business unit
+  onFilter() {
+    let filtered = this.businessUnits();
+
+    if (this.filterName !== '') {
+      const searchName = this.filterName.toLowerCase();
+      filtered = filtered.filter(asset =>
+        asset.name.toLowerCase().includes(searchName)
+      );
+    }
+
+    this.filteredBusinessUnits.set(filtered);
   }
   // Diverse funzioni che si attivano prima durante o dopo la modifica di un tipo
   OnOpenDialog(businessUnit: BusinessUnit){
     this.editableBusinessUnit = businessUnit;
     this.initialName = businessUnit.name;
+    this.controlName = businessUnit.name;
   
     this.dialog.nativeElement.showModal();
+    this.alertTitle = 'Conferma la modifica di questa Business unit?';
   }
   onModify(){
     if(this.editableBusinessUnit !== null){
@@ -86,13 +141,15 @@ export class BusinessUnitList {
     }
   }
   onCloseDialog(){
+    this.touched = false;
     const subscription = this.apiService.getBusinessUnits().subscribe({
       next: (types) => {
         this.businessUnits.set(types ?? []);
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('API error asset types', err);
+        this.errorMessage = 'API error asset types' + err;
+        this.onErrorOccur();
         this.businessUnits.set([]);
         this.loading.set(false);
       }
@@ -103,9 +160,7 @@ export class BusinessUnitList {
     this.dialog.nativeElement.close();
   }
   onConfirmEdit(){
-    const confirmed = window.confirm('Conferma modifica?');
-
-    if(this.editableBusinessUnit !== null && confirmed){
+    if(this.editableBusinessUnit !== null){
 
       const puttableBusinessUnit = { 
         name: this.editableBusinessUnit.name
@@ -117,24 +172,37 @@ export class BusinessUnitList {
           this.businessUnits.set(updatedList);
           this.dialog.nativeElement.close();
           this.initialName = '';
+          this.controlName = '';
+          this.alertTitle = '';
           this.reloadDiv();
         },
         error: (err) => {
-          console.error('Errore durante l\'aggiornamento della business unit: ', err);
+          this.errorMessage = 'Errore durante l\'aggiornamento della business unit: ' + err;
+          this.onErrorOccur();
           this.dialog.nativeElement.close();
+          this.initialName = '';
+          this.alertTitle = '';
+          this.controlName = '';
         }
       });
     }
   }
-  isInvalid(){
-    if(this.initialName.length > 2 && this.initialName.match(/^[a-zA-Z]+$/)){
-      return false;
+  isInvalid(): boolean {
+    return !(this.initialName.length > 2 && this.initialName.trim() !== this.controlName);
+  }
+  get specifiedMessage(): string {
+    if (this.initialName.length <= 2) {
+      return 'Nome non valido. Deve essere almeno 2 caratteri';
     }
-    else{ return true; }
+    return 'Nome non valido. Il nome inserito deve essere diverso da quello già presente';
+  }
+  specifiedError(): boolean {
+    return this.initialName.length > 0 && this.isInvalid();
   }
   OnOpenNewDialog(){
     this.newDialog.nativeElement.showModal();
     this.initialName = '';
+    this.alertTitle = 'Conferma la creazione di una nuova business unit?';
   }
   onAdd(){
     let cretaedName = this.initialName.toLowerCase();
@@ -143,18 +211,17 @@ export class BusinessUnitList {
     this.newBusinessUnit!.name = cretaedName;
   }
   onCloseNewDialog(){
+    this.touched = false;
     this.initialName = '';
     this.reloadDiv();
 
     this.newDialog.nativeElement.close();
   }
   onConfirmCreate(){
-    const confirmed = window.confirm('Conferma creazione?');
-
-    if(confirmed){
-      const postableBusinessUnit = { 
-        name: this.newBusinessUnit.name
-      };
+    const postableBusinessUnit = { 
+      name: this.newBusinessUnit.name
+    };
+    if(this.controlRepeatedName(postableBusinessUnit.name.trim())){
       this.apiService.postBusinessUnit(postableBusinessUnit)
       .subscribe({
         next: (createdBusinessUnit) => {
@@ -162,14 +229,23 @@ export class BusinessUnitList {
           this.businessUnits.set(updatedList);
           this.newDialog.nativeElement.close();
           this.initialName = '';
+          this.alertTitle = '';
           this.cdr.detectChanges();
           this.reloadDiv();
         },
         error: (err) => {
-          console.error('Errore durante la creazione della business unit: ', err);
+          this.errorMessage = 'Errore durante la creazione della business unit: ' + err;
+          this.onErrorOccur();
           this.newDialog.nativeElement.close();
+          this.initialName = '';
+          this.alertTitle = '';
         }
       })
+    }
+    else{
+      this.errorMessage = 'Errore nella creazione della business unit, non si può usare lo stesso nome per più business'
+      this.newDialog.nativeElement.close();
+      this.onErrorOccur();
     }
   }
   reloadDiv() {
@@ -178,5 +254,53 @@ export class BusinessUnitList {
       this.isVisible.set(true);
       this.reload$.next(true);
     }, 0);
+  }
+  controlRepeatedName(postableBusinessUnitName: string){
+    if(this.businessUnits().find(unit => (unit.name === postableBusinessUnitName))){
+      return false;
+    }
+    else{ return true; }
+  }
+  onAlertDialogOpen(){
+    this.alertDialog.nativeElement.showModal();
+  }
+ onAlertDialogClose(){
+    this.alertDialog.nativeElement.close();
+    this.alertStatusDialog.nativeElement.close();
+    this.alertTitle = '';
+    this.selectedBusinessUnit = null;
+  }
+  onAlertChangeStatus(businessUnit: BusinessUnit){
+    this.selectedBusinessUnit = businessUnit;
+    console.log(this.selectedBusinessUnit.name);
+    this.alertTitle = 'Conferma il cambiamento di status di '+ businessUnit.name +'?'
+    this.alertStatusDialog.nativeElement.showModal();
+  }
+  onChoosingPath(){
+    if(this.alertTitle === 'Conferma la creazione di una nuova business unit?'){
+      this.onConfirmCreate();
+    }
+    else{
+      if(this.alertTitle === 'Conferma la modifica di questa Business unit?'){
+        this.onConfirmEdit();
+      }
+      else{
+        if(this.alertTitle === 'Conferma il cambiamento di status di '+ this.selectedBusinessUnit!.name +'?'){
+          this.changeStatus(this.selectedBusinessUnit!);
+        }
+        else{
+          this.errorMessage = 'errore nella conferma';
+          this.onErrorOccur();
+        }
+      }
+    }
+    this.alertDialog.nativeElement.close();
+    this.alertStatusDialog.nativeElement.close();
+  }
+  onErrorOccur(){
+    this.errorDialog.nativeElement.showModal();
+  }
+  onErrorWindowsClose(){
+    this.errorDialog.nativeElement.close();
   }
 }
